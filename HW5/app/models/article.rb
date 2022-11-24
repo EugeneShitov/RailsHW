9ce62bb252ce53c044be0e13077e32c20dac787f
Evgeniy Sh <fured.rb@gmail.com>
2022-11-24 16:58:34 +0200

    edit migration and add association

--- HW5/app/models/article.rb
+++ HW5/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  has_many :authors
+  has_many :comments
 end
--- HW5/db/migrate/20221124140100_create_comments.rb
+++ HW5/db/migrate/20221124140100_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
+      t.integer :status
+      t.string :body
+      t.references :author_id
+      t.references :post_id
 
       t.timestamps
     end
