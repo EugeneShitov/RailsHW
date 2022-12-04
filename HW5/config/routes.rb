4b96c9978e98490ff31ec0407048f2855d1d4f4c
Evgeniy Sh <fured.rb@gmail.com>
2022-12-04 10:20:40 +0200

    fix routes for hw6

--- HW5/config/routes.rb
+++ HW5/config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :comments do
-        get :change_status, to: 'comments#status', on: :member
-        get :unpublished, on: :collection
-        get :published, on: :collection
-
-      end
       resources :articles do
-        get :published, on: :member
-        get :unpublished, on: :member
+        resources :comments do
+          get :published, on: :collection
+          get :unpublished, on: :collection
+          get :status, on: :member
+        end
       end
     end
   end
